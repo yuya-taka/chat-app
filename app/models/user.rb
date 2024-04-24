f4e4bdd4d5871b9b83ab5df566118ca61548db43
yuya-taka <167413446+yuya-taka@users.noreply.github.com>
2024-04-24 13:38:19 +0900

    プルダウンの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence:true
-  has_many :room_user
-  has_many :rooms, through: :room_user
+  
+  has_many :room_users
+  has_many :rooms, through: :room_users
 end
